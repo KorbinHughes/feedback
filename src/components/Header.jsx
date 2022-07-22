28e3b7bc4c8921dc0c2d31139052b3e6b5dacd73
KorbinHughes <korbinhughes@protonmail.com>
2022-07-22 15:15:33 +0430

    updated header again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({ text, bgColor, textColor }) {
   );
 }
 Header.defaultProps = {
-  text: "Hootan joon ziaad shakh nasho baradar",
+  text: "Feedback App",
   bgColor: "rgba(0,0,0,.4)",
   textColor: "#ff6a95",
 };
